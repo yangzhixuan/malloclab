034df7a10c46a654473bf399d4560ee77d61013e
Yang Zhixuan <javer821@gmail.com>
2014-12-08 11:31:56 +0800

    basic BST completed

--- mm-yzx.c
+++ mm-yzx.c
@@ -42,7 +42,7 @@
 #define WSIZE 4
 #define DSIZE 8
 #define MINIMAL_BLOCKSIZE 16
-#define CHUNKSIZE (1<<12)
+#define CHUNKSIZE (0)
 #define PACK(size, alloc) ((size) | (alloc))
 #define GET(p) (*(unsigned int *)(p))
 #define PUT(p, val) (*(unsigned int *)(p) = (val))
